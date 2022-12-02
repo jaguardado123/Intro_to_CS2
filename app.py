d691d6f69473f8df059997d39c7e158050dc12e0
Joselito <jaguardado123@gmail.com>
2022-12-02 17:35:12 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# Import libraries and packages, they contain additional functions.
+from flask import Flask, render_template
+
+# Create web application using Flask
+app = Flask(__name__)
+
+# Create our routes
+# New comment
+@app.route('/')
+def home():
+    return render_template('index.html')
